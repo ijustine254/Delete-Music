6f480b11d9b70a84c8e6511411e56337af68a19a
Justine <ontita254@gmail.com>
2022-04-26 20:20:02 +0300

    code clean up

--- main.cpp
+++ main.cpp
@@ -22,28 +22,27 @@ void listFiles(const string &path, function<void(const std::string &)> cb) {
 
 LPCWSTR drive[13] = { L"A:\\", L"B:\\", L"C:\\", L"D:\\", L"E:\\", L"F:\\", L"G:\\", L"H:\\", L"I:\\", L"J:\\",  L"K:\\", L"L:\\" };
 
-int main() {
-    listFiles("E:\\", [](const string &path) {
-        string ext, str;
-        ext = path.substr(path.find_last_of(".")+1);
-        str = "mp3 mp4 3gp mkv avi webm avi ogg wav";
-        if (str.find(ext) != string::npos) {
-            // Delete File
-            const char* patth = path.c_str();
-            remove(patth);
-        }
-        cout << ext << std::endl;
-    });
 
-//    UINT drives_num;
-//    int i;
-//    for (i = 0; i < 12; i++) {
-//        drives_num = GetDriveType(reinterpret_cast<LPCSTR>(drive[i]));
-//        cout << drives_num << endl;
-//        if (drives_num == DRIVE_FIXED) {
-//            //cout << "Drives: " << drive[i] << endl;
-//        }
-//    }
+int main() {
+
+    UINT drives_num;
+    int i;
+    for (i = 0; i < 12; i++) {
+        drives_num = GetDriveType(reinterpret_cast<LPCSTR>(drive[i]));
+        if (drives_num == DRIVE_FIXED) {
+            // TODO: Pass Drive to listFiles func
+            listFiles("", [](const string &path) {
+                string ext, str;
+                ext = path.substr(path.find_last_of('.')+1);
+                str = "mp3 mp4 3gp mkv avi webm avi ogg wav";
+                if (str.find(ext) != string::npos) {
+                    // Delete File
+                    const char* patth = path.c_str();
+                    remove(patth);
+                }
+            });
+        }
+    }
     return 0;
 }
 
